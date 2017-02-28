16439dbb4a699e759fc35c13becad5c6eeca4973
Genroa <genroa@hotmail.fr>
2017-02-28 20:19:50 +0100

    package desc

--- package.js
+++ package.js
@@ -2,12 +2,12 @@ Package.describe({
 	name: 'genroa:nb-ct-text',
 	version: '0.0.1',
 	// Brief, one-line summary of the package.
-	summary: '',
+	summary: 'Formated Text content type package for Nebula Builder',
 	// URL to the Git repository containing the source code for this package.
 	git: '',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
-	documentation: 'README.md'
+	documentation: null
 });
 
 Package.onUse(function(api) {
